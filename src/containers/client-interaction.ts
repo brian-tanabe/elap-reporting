cc4eb01e752f9d3d541ad0df85d550ca4d2057f0
Brian Tanabe <btanabe2@gmail.com>
2020-02-23 21:46:50 -0800

    Implementing the ClientInteraction class and adding tests.

---
+++ src/containers/client-interaction.ts
@@ -0,0 +1,101 @@
+/**
+ *
+ */
+export class ClientInteraction {
+    private readonly _reportingMonth: Date;
+    private readonly _openDate: Date;
+    private readonly _clientName: String;
+    private readonly _attorneyName: String;
+    private readonly _legalServerNumber: String;
+    private readonly _closedDate: Date;
+    private readonly _typeOfService: String;
+    private readonly _courtAppearances: Number;
+    private readonly _status: String;
+
+    constructor(
+        reportingMonth: Date,
+        openDate: Date,
+        clientName: String,
+        attorneyName: String,
+        legalServerNumber: String,
+        closedDate: Date,
+        typeOfService: String,
+        courtAppearances: Number,
+        status: String
+    ) {
+        this.validateClientInteraction(reportingMonth, attorneyName, clientName, openDate, courtAppearances);
+
+        this._reportingMonth = reportingMonth;
+        this._openDate = openDate;
+        this._clientName = clientName;
+        this._attorneyName = attorneyName;
+        this._legalServerNumber = legalServerNumber;
+        this._closedDate = closedDate;
+        this._typeOfService = typeOfService;
+        this._courtAppearances = courtAppearances;
+        this._status = status;
+    }
+
+    private validateClientInteraction(reportingMonth: Date, attorneyName: String, clientName: String, openDate: Date, courtAppearances: Number) {
+        if (!reportingMonth) {
+            throw new Error("A ClientInteraction cannot have a null reporting month!");
+        }
+
+        if (!attorneyName) {
+            throw new Error("A ClientInteraction cannot have a null attorney name!");
+        }
+
+        if (!clientName) {
+            throw new Error("A ClientInteraction cannot have a null client name!");
+        }
+
+        if(!openDate) {
+            throw new Error("A ClientInteraction cannot have a null open date!");
+        }
+
+        if(!courtAppearances) {
+            throw new Error("A ClientInteraction cannot have a null court appearances!");
+        }
+    }
+
+    toString(): String {
+        return "ClientInteraction reportingMonth=[" + this._reportingMonth + "], attorney=[" + this._attorneyName + "], name=[" + this._clientName + "], openDate=[" + this._openDate + "]";
+    }
+
+    // Getters
+    get reportingMonth(): Date {
+        return this._reportingMonth;
+    }
+
+    get openDate(): Date {
+        return this._openDate;
+    }
+
+    get clientName(): String {
+        return this._clientName;
+    }
+
+    get attorneyName(): String {
+        return this._attorneyName;
+    }
+
+    get legalServerNumber(): String {
+        return this._legalServerNumber;
+    }
+
+    get closedDate(): Date {
+        return this._closedDate;
+    }
+
+    get typeOfService(): String {
+        return this._typeOfService;
+    }
+
+    get courtAppearances(): Number {
+        return this._courtAppearances;
+    }
+
+    get status(): String {
+        return this._status;
+    }
+}
---
+++ test/src/containers/client-interaction-spec.ts
@@ -0,0 +1,165 @@
+import 'mocha';
+
+import {ClientInteraction} from '../../../src/containers/client-interaction';
+import {suite, test} from "mocha-typescript";
+
+const assert = require('assert');
+const expect = require('chai').expect;
+
+const ATTORNEY_NAME: String = "Ailise Delaney";
+const CLIENT_NAME: String = "Brian Tanabe";
+const SIMPLE_LEGAL_SERVER_NUMBER: String = "20-0055166";
+const COMPLEX_LEGAL_SERVER_NUMBER: String = "20-0055178 / 20-0055179";
+const TYPE_OF_SERVICE_LIMITED_REPRESENTATION: String = "Limited Representation";
+const TYPE_OF_SERVICE_EXTENDED_SERVICES: String = "Extended Services";
+const STATUS_OPEN: String = "Open";
+const STATUS_CLOSED: String = "Closed";
+const CLOSED_DATE_NULL: Date = null;
+
+@suite
+class ClientInteractionSpec {
+
+    @test
+    shouldBeAbleToCreateAClientInteractionWithNullClosedDate() {
+        const reportingMonth: Date = new Date(Date.UTC(2019, 1, 1).valueOf());
+        const clientOpenDate: Date = new Date(Date.UTC(2019, 2, 14).valueOf());
+
+        let testClientInteraction = new ClientInteraction(
+            reportingMonth,
+            clientOpenDate,
+            CLIENT_NAME,
+            ATTORNEY_NAME,
+            SIMPLE_LEGAL_SERVER_NUMBER,
+            CLOSED_DATE_NULL,
+            TYPE_OF_SERVICE_EXTENDED_SERVICES,
+            2,
+            STATUS_OPEN
+        );
+
+        this.verifyClientInteraction(
+            testClientInteraction,
+            new Date(Date.UTC(2019, 1, 1).valueOf()),
+            new Date(Date.UTC(2019, 2, 14).valueOf()),
+            CLIENT_NAME,
+            ATTORNEY_NAME,
+            SIMPLE_LEGAL_SERVER_NUMBER,
+            CLOSED_DATE_NULL,
+            TYPE_OF_SERVICE_EXTENDED_SERVICES,
+            2,
+            STATUS_OPEN
+        );
+    }
+
+    @test
+    shouldNotConsiderClientInteractionsWithNullReportingMonthsValid() {
+        const invalidReportingMonth: Date = null;
+        const clientOpenDate: Date = new Date(Date.UTC(2019, 2, 14).valueOf());
+
+        expect(() => {
+            new ClientInteraction(
+                invalidReportingMonth,
+                clientOpenDate,
+                CLIENT_NAME,
+                ATTORNEY_NAME,
+                SIMPLE_LEGAL_SERVER_NUMBER,
+                CLOSED_DATE_NULL,
+                TYPE_OF_SERVICE_EXTENDED_SERVICES,
+                2,
+                STATUS_OPEN
+            );
+        }).to.throw("A ClientInteraction cannot have a null reporting month!");
+    }
+
+    @test
+    shouldNotConsiderClientInteractionsWithNullAttorneyValid() {
+        const reportingMonth: Date = new Date(Date.UTC(2019, 1, 1).valueOf());
+        const clientOpenDate: Date = new Date(Date.UTC(2019, 2, 14).valueOf());
+
+        const invalidAttorneyName: String = null;
+
+        expect(() => {
+            new ClientInteraction(
+                reportingMonth,
+                clientOpenDate,
+                CLIENT_NAME,
+                invalidAttorneyName,
+                SIMPLE_LEGAL_SERVER_NUMBER,
+                CLOSED_DATE_NULL,
+                TYPE_OF_SERVICE_EXTENDED_SERVICES,
+                2,
+                STATUS_OPEN
+            );
+        }).to.throw("A ClientInteraction cannot have a null attorney name!");
+    }
+
+    @test
+    shouldNotConsiderClientInteractionsWithNullClientValid() {
+        const reportingMonth: Date = new Date(Date.UTC(2019, 1, 1).valueOf());
+        const clientOpenDate: Date = new Date(Date.UTC(2019, 2, 14).valueOf());
+
+        const invalidClientName: String = null;
+
+        expect(() => {
+            new ClientInteraction(
+                reportingMonth,
+                clientOpenDate,
+                invalidClientName,
+                ATTORNEY_NAME,
+                SIMPLE_LEGAL_SERVER_NUMBER,
+                CLOSED_DATE_NULL,
+                TYPE_OF_SERVICE_EXTENDED_SERVICES,
+                2,
+                STATUS_OPEN
+            );
+        }).to.throw();
+    }
+
+    @test
+    shouldNotConsiderClientInteractionsWithNullCourtAppearancesValid() {
+        const reportingMonth: Date = new Date(Date.UTC(2019, 1, 1).valueOf());
+        const clientOpenDate: Date = new Date(Date.UTC(2019, 2, 14).valueOf());
+
+        const invalidCourtAppearances: Number = null;
+
+        expect(() => {
+            new ClientInteraction(
+                reportingMonth,
+                clientOpenDate,
+                CLIENT_NAME,
+                ATTORNEY_NAME,
+                SIMPLE_LEGAL_SERVER_NUMBER,
+                CLOSED_DATE_NULL,
+                TYPE_OF_SERVICE_EXTENDED_SERVICES,
+                invalidCourtAppearances,
+                STATUS_OPEN
+            );
+        }).to.throw();
+    }
+
+    private verifyClientInteraction(
+        testClientInteraction: ClientInteraction,
+        expectedReportingMonth: Date,
+        expectedOpenDate: Date,
+        expectedClientName: String,
+        expectedAttorneyName: String,
+        expectedLegalServerNumber: String,
+        expectedClosedDate: Date,
+        expectedTypeOfService: String,
+        expectedCourtAppearances: Number,
+        expectedStatus: String
+    ) {
+
+        // I would like to use BDD here but I can't figure out how to do deep equality checks needed
+        // for date comparisons with the expect-equals syntax
+        assert.deepStrictEqual(testClientInteraction.reportingMonth, expectedReportingMonth);
+        assert.deepStrictEqual(testClientInteraction.openDate, expectedOpenDate);
+        assert.strictEqual(testClientInteraction.clientName, expectedClientName);
+        assert.strictEqual(testClientInteraction.attorneyName, expectedAttorneyName);
+        assert.strictEqual(testClientInteraction.legalServerNumber, expectedLegalServerNumber);
+        assert.strictEqual(testClientInteraction.closedDate, expectedClosedDate);
+        assert.strictEqual(testClientInteraction.typeOfService, expectedTypeOfService);
+        assert.strictEqual(testClientInteraction.courtAppearances, expectedCourtAppearances);
+        assert.strictEqual(testClientInteraction.status, expectedStatus);
+    }
+
+}
